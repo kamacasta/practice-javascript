f6eb6fd5093e3cb279a342094defb06f4634d745
kama casta <kamacasta983@gmail.com>
2021-08-23 17:10:28 -0700

    added attack phase between both robots need to add more than one more round

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,6 +17,14 @@ var fight = function() {
     console.log(
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
+    
+    // Check enemy's health
+    if(enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
 
     // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
     playerHealth = playerHealth - enemyAttack;
@@ -26,7 +34,13 @@ var fight = function() {
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
 
-    
+    // Check your robot's health 
+    if(playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 
   };
 fight();
